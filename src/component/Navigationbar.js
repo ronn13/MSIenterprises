23da3c2584103e86b645aea658ef43a55aa43360
surajsly <50519406+surajsly@users.noreply.github.com>
2020-12-28 09:06:57 +0530

    navigation bar and carosal

---
+++ src/component/Navigationbar.js
@@ -0,0 +1,143 @@
+import React from "react";
+import "./Navigationbar.css";
+
+function Navigationbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            M S I
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div
+        id="carouselExampleIndicators"
+        className="carousel slide"
+        data-ride="carousel"
+      >
+        <ol className="carousel-indicators">
+          <li
+            data-target="#carouselExampleIndicators"
+            data-slide-to="0"
+            className="active"
+          ></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              className="d-block w-100"
+              src="https://images.unsplash.com/photo-1509391366360-2e959784a276?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=752&q=80"
+              alt="First slide"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>MSI ENTERPRIESES</h5>
+              <p className="d-none d-md-block">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Maxime, nulla, tempore. Deserunt excepturi quas vero.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              className="d-block w-100"
+              src="https://images.unsplash.com/photo-1521618755572-156ae0cdd74d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=756&q=80"
+              alt="Second slide"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>MSI ENTERPRIESES</h5>
+              <p className="d-none d-md-block">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Maxime, nulla, tempore. Deserunt excepturi quas vero.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              className="d-block w-100"
+              src="https://images.unsplash.com/photo-1574360773950-133867861ae9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+              alt="Third slide"
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>MSI ENTERPRIESES</h5>
+              <p className="d-none d-md-block">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Maxime, nulla, tempore. Deserunt excepturi quas vero.
+              </p>
+            </div>
+          </div>
+        </div>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Navigationbar;
